8e6c0e907f8600425b367dea540b34f9493b5d26
BrianGomezM <brayan.gomez2646@gmail.com>
2025-02-25 23:36:09 -0500

    Agrega información sobre la Segunda Guerra Mundial
    Se añadió un párrafo informativo al componente Footer, detallando el impacto y contexto histórico de la Segunda Guerra Mundial. Esto enriquece el contenido mostrado al usuario.

--- Cliente/src/components/Footer.js
+++ Cliente/src/components/Footer.js
@@ -18,6 +18,14 @@ const Footer = () => {
                             involucrando a las principales potencias mundiales y dejando una profunda huella en la
                             historia de la humanidad.
                         </p>
+                        <p>
+                            <p>
+                                La Segunda Guerra Mundial, que tuvo lugar entre 1939 y 1945, fue uno de los conflictos
+                                más devastadores de la historia, marcada por la lucha entre los Aliados y las Potencias
+                                del Eje, dejando un impacto profundo en la política y sociedad mundial.
+                            </p>
+                        </p>
+
                     </p>
                     <a href="https://www.linkedin.com/in/brayan-julio-g%C3%B3mez-mu%C3%B1oz-b195b818b" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
                         <FaLinkedin size={24} />
